b30157e132340db8aa90a5ac8f9ca5c4ba6b9c10
AugAIDataScientist <usamaaugai@gmail.com>
2025-06-23 12:39:19 +0500

    main.py

--- main.py
+++ main.py
@@ -87,7 +87,7 @@ with st.sidebar:
 @st.cache_resource
 def init_mongodb():
     try:
-        MONGO_URI = "mongodb://localhost:27017"
+        MONGO_URI = "mongodb+srv://usamaaugai:RJ4nZKo6czV5nQVj@cluster0.9cf3x5j.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
         client = MongoClient(MONGO_URI)
         db = client["car_plate_db"]
         collection = db["plate_records"]
